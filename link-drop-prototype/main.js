400b28338a5ee211df4f1862b57c182c28d8ba4c
Robert Rees <shudderfix@gmail.com>
2024-08-28 23:06:37 +0100

    Working read of the form data

--- link-drop-prototype/main.js
+++ link-drop-prototype/main.js
@@ -3,7 +3,10 @@ import { setupCounter } from './counter.js'
 
 document.addEventListener('submit', (event) => {
   event.preventDefault();
-  console.log('Form save');
+  const data = new FormData(event.target);
+  console.log('Form save', event.target);
+  console.log(data.getAll('url').pop(), data.getAll('notes').pop());
+
 });
 
 setupCounter(document.querySelector('#counter'))
